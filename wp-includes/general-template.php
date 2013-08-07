a8423b6653e6cb0a5b72a3c0b508652d01558521
johnregan3 <john.m.regan@gmail.com>
2013-08-07 00:36:00 -0500

    MYSQL2DATE Added extra  to get_post_modified_time

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -1554,7 +1554,7 @@ function get_post_modified_time( $d = 'U', $gmt = false, $post = null, $translat
 		$time = $post->post_modified_gmt;
 	else
 		$time = $post->post_modified;
-	$time = mysql2date($d, $time, $translate);
+	$time = mysql2date($d, $time, $translate, $gmt);
 
 	return apply_filters('get_post_modified_time', $time, $d, $gmt);
 }
